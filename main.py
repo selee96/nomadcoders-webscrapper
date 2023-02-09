f787f53eeb0cc797e6f138132e6c08c61fc9dda0
selee <tldms296@gmail.com>
2023-02-09 18:18:46 +0900

    python game

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-def tax_calculator(money):
-    print(money * 0.35)
+from random import randint
 
+print("Welcome to Python Casino")
 
-tax_calculator(40000000)
+pc_choice = randint(1, 100)
+
+playing = True
+
+while playing:
+    user_choice = int(input("Choose number (1-100):"))
+    if user_choice == pc_choice:
+        print("You won!")
+        playing = False
+    elif user_choice > pc_choice:
+        print("Lower!")
+    elif user_choice < pc_choice:
+        print("Higher!")
